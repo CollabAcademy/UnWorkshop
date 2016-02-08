0af019c944e2fe2ceb145a20a3d405639423ca77
akshithg <akshith@getsimpl.com>
2016-02-08 23:34:21 +0530

    migrating to rails, cleanup

--- admins.js
+++
@@ -1,4 +0,0 @@
-exports.ROUND_ONE = process.env.ADMIN_ROUND_ONE;
-exports.ROUND_TWO = process.env.ADMIN_ROUND_TWO;
-exports.ROUND_THREE = process.env.ADMIN_ROUND_THREE;
-exports.LIST = [this.ROUND_ONE, this.ROUND_TWO];
